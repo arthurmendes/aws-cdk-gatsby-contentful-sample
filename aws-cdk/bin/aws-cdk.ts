39d80c0003656e99b0432309fa2b5e54c2f480db
Arthur Mendes Lorena <arthurmlorena@gmail.com>
2023-03-03 22:16:59 -0500

    Configure CDK to use default account and region

--- aws-cdk/bin/aws-cdk.ts
+++ aws-cdk/bin/aws-cdk.ts
@@ -5,17 +5,14 @@ import { AwsCdkStack } from '../lib/aws-cdk-stack';
 
 const app = new cdk.App();
 new AwsCdkStack(app, 'AwsCdkStack', {
-  env: {
-    region: 'us-east-1'
-  },
-  domainName: 'aws-cdk-gatsby-contentful-sample.ca'
+  domainName: 'aws-cdk-gatsby-contentful-sample.ca',
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
