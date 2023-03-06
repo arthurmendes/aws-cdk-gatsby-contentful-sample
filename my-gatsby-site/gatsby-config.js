be9066b39031385dca2fb0a2bc0d576be9f3639b
Arthur Mendes Lorena <arthurmlorena@gmail.com>
2023-03-05 19:23:07 -0500

    Remove Contentful credentials

--- my-gatsby-site/gatsby-config.js
+++ my-gatsby-site/gatsby-config.js
@@ -2,27 +2,36 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "LSVSCOTAXAyicuSy5Gt07D0VpYKj93gzT752isFIkog",
-      "spaceId": "t1jnhpbgev7w"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `My Gatsby Site`,
+        siteUrl: `https://www.yourdomain.tld`,
     },
-    __key: "images"
-  }]
+    plugins: [
+        {
+            resolve: "gatsby-source-contentful",
+            options: {
+                accessToken: "",
+                spaceId: "",
+            },
+        },
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        "gatsby-plugin-styled-components",
+        "gatsby-plugin-sitemap",
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                icon: "src/images/icon.png",
+            },
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+    ],
 };
